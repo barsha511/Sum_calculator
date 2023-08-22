eb1a3fe2081c7142a2649ef2213b7840a0675143
Barsha singh <varshagautam511@gmail.com>
2023-08-23 00:06:16 +0530

    sum calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './index.css'
+import React, {useState} from 'react'
+ 
 function App() {
+ 
+  // state
+  const [num1,setNum1] = useState(0)
+  const [num2,setNum2] = useState(0)
+  const [sum,setSum] = useState('')
+ 
+  let calculator = (event) => {
+    //prevent submitting to the server
+    event.preventDefault()
+ 
+    if (num1 === 0 || num2 === 0) {
+      alert('Please enter a valid weight and height')
+    }
+    else {
+      let sum = parseInt(num1) + parseInt(num2);
+      setSum(sum.toFixed(1))
+    }
+  }
+  let reload = () => {
+    window.location.reload()
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className='container'>
+      <h2 className='center'>Sum Calculator</h2>
+      <form onSubmit={calculator}>
+ 
+        <div>
+          <label>num1</label>
+          <input value={num1} onChange={(e) => setNum1(e.target.value)} />
+        </div>
+ 
+        <div>
+          <label>num2</label>
+          <input value={num2} onChange={(event) => setNum2(event.target.value)} />
+        </div>
+ 
+        <div>
+          <button className='btn' onClick={calculator}>Add</button>
+          <button className='btn btn-outline' onClick={reload}>Reload</button>
+        </div>
+      </form>
+ 
+      <div className='center'>
+        <h3>Your Summation is: {sum}</h3>
+        
+      </div>
     </div>
+  </div>
   );
 }
-
+ 
 export default App;
